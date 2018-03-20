7cf2b992a81c90cb348bb299f5b3eddf6137a875
Teresa <teresa.lenz@desy.de>
2018-03-20 18:54:04 +0100

    only one mht bin

--- PlotTrigger.C
+++ PlotTrigger.C
@@ -9,6 +9,7 @@ TGraphAsymmErrors * ComputeTriggerEfficiency(TTree * tree,
 					     TString cutsDen,
 					     TString histName) {
 
+
   TCanvas * dummy = new TCanvas("dummy","",400,400);
   TString histNameNum = histName + "_Num";
   TString histNameDen = histName + "_Den";
@@ -26,79 +27,56 @@ TGraphAsymmErrors * ComputeTriggerEfficiency(TTree * tree,
 
 }
 
-Double_t FitFunc(Double_t * x,
-		 Double_t * par) {
+Double_t FitFunc(Double_t * x,Double_t * par) {
   
   Double_t a1 = (x[0] - par[1])/par[2];
   Double_t a2 = (x[0] - par[3])/par[4];
 
   return par[0]*(par[5]*TMath::Erf(a1)+(1-par[5])*TMath::Erf(a2));
-
-  
+ 
 }
 
 void PlotTrigger() {
 
-  TString numerator   = "IsW>0.5&&trigger>0.5";
-  TString denominator = "IsW>0.5";
-  TString mhtRange[4] = {"&&mhtNoMu<130",
-			 "&&mhtNoMu>=130","",""};
+  TString numerator   = "IsW>0.5&&mtmuon>50&&trigger>0.5";
+  TString denominator = "IsW>0.5&&mtmuon>50";
+  //TString mhtRange[4] = {"&&mhtNoMu<120","&&mhtNoMu>=120","",""};
+  TString mhtRange[4] = {"&&mhtNoMu>120","","",""};
   
-  TString bins[4] = {"MhtLt130",
-		     "MhtGt130","",""};
+  TString bins[4] = {"MhtLt120","MhtGt120","",""};
 
-  TString HTlabel[2] = {"H_{T,no#mu} < 130 GeV",
-			"H_{T,no#mu} > 130 GeV"};
+  TString HTlabel[2] = {"H_{T,no#mu} < 120 GeV","H_{T,no#mu} > 120 GeV"};
 
   SetStyle();
   TH1::SetDefaultSumw2();
   TH2::SetDefaultSumw2();
 
-  TFile * fileData = new TFile(dir+"/SingleMuon_Run2016.root");
-  TFile * fileMC   = new TFile(dir+"/WJetsToLNu_13TeV-madgraphMLM.root");
+  TFile * fileData = new TFile(dir+"/SingleMuon_Run2017.root");
+  TFile * fileMC   = new TFile(dir+"/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
 
-  TTree * ntupleData = (TTree*)fileData->Get("TriggerNTuple");
-  TTree * ntupleMC = (TTree*)fileMC->Get("TriggerNTuple");
 
-  int nBins = 40;
-  float xbins[100];
-  for (int i=0; i<=40; ++i)
-    xbins[i] = float(i)*10.;
-  float xmin = 90;
-  float xmax = 400;
+  TTree * ntupleData = (TTree*)fileData -> Get("TriggerNTuple");
+  TTree * ntupleMC   = (TTree*)fileMC   -> Get("TriggerNTuple");
 
+  const int nBins = 17;
+  float xbins[nBins+1] = {120,140,160,180,200,220,240,260,280,300,340,380,420,460,500,600,700,800};
   
   TGraphAsymmErrors * effData[4];
   TGraphAsymmErrors * effMC[4];
   TF1 * dataFunc[4];
   TF1 * mcFunc[4];
   
-  for (int i=0; i<2; ++i) {
+  for (int i=0; i<1; i++) {
 
     TString cutsNum = numerator+mhtRange[i];
     TString cutsDen = denominator+mhtRange[i];
 
-    if (i==0) {
-      xmin = 100;
-      xmax = 300;
-    }
-    else {
-      xmin = 90;
-      xmax = 400;
-    }
-
-    effData[i] = ComputeTriggerEfficiency(ntupleData,
-					  "metNoMu",
-					  nBins,
-					  xbins,
-					  cutsNum,
-					  cutsDen,
-					  "DataEff"+bins[i]);
+    effData[i] = ComputeTriggerEfficiency(ntupleData,"metNoMu",nBins,xbins,cutsNum,cutsDen,"DataEff"+bins[i]);
 
     effData[i]->SetMarkerStyle(20);
     effData[i]->SetMarkerColor(1);
     effData[i]->SetMarkerSize(0.7);
-    dataFunc[i] = new TF1("dataFunc"+bins[i],FitFunc,xmin,xmax,6);
+    dataFunc[i] = new TF1("dataFunc"+bins[i],FitFunc,xbins[0],xbins[nBins],6);
     dataFunc[i]->SetParameter(0,1.);
     dataFunc[i]->SetParameter(1,80.);
     dataFunc[i]->SetParameter(2,50.);
@@ -108,8 +86,7 @@ void PlotTrigger() {
     dataFunc[i]->SetParLimits(5,0.01,0.99);
     dataFunc[i]->SetLineColor(1);
     effData[i]->Fit("dataFunc"+bins[i],"R");
-  
-
+ 
     effMC[i] = ComputeTriggerEfficiency(ntupleMC,
 					"metNoMu",
 					nBins,
@@ -122,7 +99,7 @@ void PlotTrigger() {
     effMC[i]->SetMarkerColor(2);
     effMC[i]->SetLineColor(2);
     effMC[i]->SetMarkerSize(1);
-    mcFunc[i] = new TF1("mcFunc"+bins[i],FitFunc,xmin,xmax,6);
+    mcFunc[i] = new TF1("mcFunc"+bins[i],FitFunc,xbins[0],xbins[nBins],6);
     mcFunc[i]->SetParameter(0,1.);
     mcFunc[i]->SetParameter(1,80.);
     mcFunc[i]->SetParameter(2,50.);
@@ -138,12 +115,13 @@ void PlotTrigger() {
     effData[i]->GetXaxis()->SetTitle("E_{T,no#mu}^{mis} [GeV]");
     effData[i]->GetYaxis()->SetTitleOffset(1.2);
     effData[i]->GetXaxis()->SetTitleOffset(1.1);
+    effData[i]->SetMaximum(1.0);
+    effData[i]->SetMinimum(0.0);
     effData[i]->Draw("ap");
     effMC[i]->Draw("psame");
 
     TLegend * leg = NULL; 
-    if (i==0) leg = new TLegend(0.2,0.65,0.6,0.9);
-    else leg = new TLegend(0.5,0.2,0.9,0.45);
+    leg = new TLegend(0.6,0.2,0.9,0.45);
     SetLegendStyle(leg);
     leg->SetTextSize(0.05);
     leg->SetHeader("single #mu events, ");
@@ -152,7 +130,7 @@ void PlotTrigger() {
     leg->AddEntry(effMC[i],"simulation","lp");
     leg->Draw();
     canv->Update();
-    canv->Print("figures/trigger_"+bins[i]+".pdf","Portrait pdf");
+    //canv->Print("figures/trigger_"+bins[i]+".pdf","Portrait pdf");
     canv->Print("figures/trigger_"+bins[i]+".png");
 
   }
