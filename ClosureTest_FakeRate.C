c1f8c564e3efb5b612b03967351969a5b0007a09
Teresa <teresa.lenz@desy.de>
2018-04-12 10:59:32 +0200

    ratio is plotted as pred/obs

--- ClosureTest_FakeRate.C
+++ ClosureTest_FakeRate.C
@@ -66,16 +66,16 @@ void ClosureTest_FakeRate() {
     TH1::SetDefaultSumw2();
     TH2::SetDefaultSumw2();
 
-    TString var = "tauMass";
+    TString var = "tauPt";
 
     //const int nBins = 10;
     //double bins[nBins+1] = {0 , 0.4 , 0.5 , 0.6 , 0.7 , 0.75 , 0.8 , 0.85 , 0.9 , 0.95 , 2.};
-    const int nBins  = 7;
-    //float bins[nBins+1] = {100,150,200,250,300,500};  // tauPt binning
+    const int nBins  = 5;
+    float bins[nBins+1] = {100,150,200,250,300,500};  // tauPt binning
     //float bins[nBins+1] = {100,200,300,400,500,700};  // met binning
-    //float bins[nBins+1] = {100,170,240,310,400,700};  // tauJetPt binning
+    //float bins[nBins+1] = {100,170,220,350,500,1200};  // tauJetPt binning
     //float bins[nBins+1] = {200,300,400,500,600,800};  // mttau binning
-    float bins[nBins+1] = {0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8};  // tauMass binning
+    //float bins[nBins+1] = {0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8};  // tauMass binning
     //float bins[nBins+1] = {0,0.5,1.1,2.3}; //tauEta binning
     TH1D * observation = new TH1D("observation_"+iso[idx_iso],"",nBins,bins); 
     TH1D * prediction  = new TH1D("prediction_"+iso[idx_iso],"",nBins,bins); 
@@ -148,14 +148,14 @@ void ClosureTest_FakeRate() {
     upper->Update();
     canv1->cd();
 
-    TH1D * ratioH = (TH1D*)observation->Clone("ratioH");
-    ratioH->Divide(prediction);
+    TH1D * ratioH = (TH1D*)prediction->Clone("ratioH");
+    ratioH->Divide(observation);
     ratioH->SetMarkerColor(1);
     ratioH->SetMarkerStyle(20);
     ratioH->SetMarkerSize(1.2);
     ratioH->SetLineColor(1);
     ratioH->GetYaxis()->SetRangeUser(0.0,2.);
-    ratioH->GetYaxis()->SetTitle("obs/pred");
+    ratioH->GetYaxis()->SetTitle("pred/obs");
     ratioH->GetXaxis()->SetTitle("");
     ratioH->GetYaxis()->CenterTitle();
     // ------------>Primitives in pad: lower
