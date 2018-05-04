0df82e1abf8f493a9c99af80b147654abc4dbb09
Teresa <teresa.lenz@desy.de>
2018-05-04 21:34:49 +0200

    script to make filelists

---
+++ NTuples/makeFilelists.sh
@@ -0,0 +1,18 @@
+#!bin/sh
+
+dir=/nfs/dust/cms/group/higgs-kit/rasp/MC_2017
+
+for subdir in $dir/*; do
+    if [[ ! -d $subdir ]] ; then
+	continue
+    fi
+    
+    #echo $subdir 'is a directory'
+
+    samplename=$(basename $subdir)
+    echo 'Sample is : ' $samplename
+
+    find $subdir/. -name "*.root" > $samplename 
+
+done
+
