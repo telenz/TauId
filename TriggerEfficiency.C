4d4a5f053bf5e6a761375da0ea125acfe32e6c64
Teresa <teresa.lenz@desy.de>
2018-05-04 16:32:20 +0200

    mewsure trigger eff for PFMETNoMu... trigger

--- TriggerEfficiency.C
+++ TriggerEfficiency.C
@@ -24,8 +24,6 @@ void TriggerEfficiency() {
   TH1::SetDefaultSumw2();
   TH2::SetDefaultSumw2();
 
-  double luminosity = 40400;
-
   std::vector< std::pair<TString,std::vector<TString>> > samples;
 
   std::vector<TString> MC;
@@ -34,10 +32,11 @@ void TriggerEfficiency() {
   MC.push_back("W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8");
   MC.push_back("W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8");
   MC.push_back("W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8");
-  // MC.push_back("W1JetsToLNu_LHEWpT_100-150");
-  // MC.push_back("W1JetsToLNu_LHEWpT_150-250");
-  // MC.push_back("W1JetsToLNu_LHEWpT_250-400");
-  // MC.push_back("W1JetsToLNu_LHEWpT_400-inf");
+  MC.push_back("W1JetsToLNu_LHEWpT_100-150");
+  MC.push_back("W1JetsToLNu_LHEWpT_150-250");
+  MC.push_back("W1JetsToLNu_LHEWpT_250-400");
+  MC.push_back("W1JetsToLNu_LHEWpT_400-inf");
+  MC.push_back("WToMuNu_M-200_TuneCP5_13TeV-pythia8");
   std::vector<TString> Data;
   Data.push_back("SingleMuon_Run2017");
   samples.push_back(make_pair("MC"   , MC));
@@ -45,8 +44,9 @@ void TriggerEfficiency() {
 
   std::map<TString,TH1D*> histoMap;
 
-  const int nbins  = 19;
-  float bins[nbins+1] = {60,90,100,110,120,130,140,150,160,170,180,190,200,220,240,280,320,380,480,600};
+
+  Float_t bins[] = {60,90,100,110,120,130,140,150,160,170,180,190,200,220,240,280,320,380,480,600,800};
+  const int nbins  = sizeof(bins)/sizeof(Float_t) - 1;
 
   // Needed for stitching
   double xsecIncl = xsecs["WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"];
@@ -54,11 +54,21 @@ void TriggerEfficiency() {
   double xsec2Jet = xsecs["W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"];
   double xsec3Jet = xsecs["W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"];
   double xsec4Jet = xsecs["W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"];
+  double xsec1JetLHEWpT_100to150 = xsecs["W1JetsToLNu_LHEWpT_100-150"];
+  double xsec1JetLHEWpT_150to250 = xsecs["W1JetsToLNu_LHEWpT_150-250"];
+  double xsec1JetLHEWpT_250to400 = xsecs["W1JetsToLNu_LHEWpT_250-400"];
+  double xsec1JetLHEWpT_400toinf = xsecs["W1JetsToLNu_LHEWpT_400-inf"];
+  double xsecWToMuNu_M200 = xsecs["WToMuNu_M-200_TuneCP5_13TeV-pythia8"];
   double nevtsProcessedIncl = getNEventsProcessed(dir+"WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed1Jet = getNEventsProcessed(dir+"W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed2Jet = getNEventsProcessed(dir+"W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed3Jet = getNEventsProcessed(dir+"W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed4Jet = getNEventsProcessed(dir+"W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
+  double nevtsProcessed1JetLHEWpT_100to150 = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_100-150.root");
+  double nevtsProcessed1JetLHEWpT_150to250 = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_150-250.root");
+  double nevtsProcessed1JetLHEWpT_250to400 = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_250-400.root");
+  double nevtsProcessed1JetLHEWpT_400toinf = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_400-inf.root");
+  double nevtsProcessedWToMuNu_M200 = getNEventsProcessed(dir+"WToMuNu_M-200_TuneCP5_13TeV-pythia8.root");
 
   for (unsigned int i=0; i<samples.size(); ++i) {
 
@@ -81,40 +91,62 @@ void TriggerEfficiency() {
       TTreeReaderValue< Bool_t  >  IsW(              *myReader,       "IsW");
       TTreeReaderValue< Bool_t  >  trigger(          *myReader,       "trigger");
       TTreeReaderValue< UInt_t  >  npartons(         *myReader,       "npartons");
+      TTreeReaderValue< UInt_t  >  npartonsNLO(      *myReader,       "npartonsNLO");
+      TTreeReaderValue< Float_t >  lheWPt(           *myReader,       "lheWPt");
       TTreeReaderValue< Float_t >  mtmuon(           *myReader,       "mtmuon");
       TTreeReaderValue< Float_t >  muonPt(           *myReader,       "muonPt");
       TTreeReaderValue< Float_t >  muonEta(          *myReader,       "muonEta");
       TTreeReaderValue< Float_t >  dPhiMetMuon(      *myReader,       "dPhiMetMuon");
+      TTreeReaderValue< Float_t >  WMass(            *myReader,       "WMass");
+      TTreeReaderValue< Float_t >  puWeight(         *myReader,       "puWeight");
+      TTreeReaderValue< Float_t >  genWeight(        *myReader,       "genWeight");
+
 
       cout<<"---------- Processing  "<<samples[i].second[idx_list]<<"  ---------- "<<endl;
 
       while(myReader->Next()){
 
-	//if(*IsW != true)   continue; // this includes an mtmuon cut of 40GeV
-	//if(*mtmuon < 50)   continue;
-	
-	double weight = 1.;
-	double norm = 1.;
+	double weight = (*genWeight)*(*puWeight);
+	double norm   = 1.;
+
+	// STITCHING
 	if(samples[i].first.Contains("MC") && samples[i].second.size()>1){
-	  if(*npartons == 0)      norm=luminosity/( nevtsProcessedIncl/xsecIncl );
-	  else if(*npartons == 1) norm=luminosity/( nevtsProcessed1Jet/xsec1Jet + nevtsProcessedIncl/xsecIncl );
-	  else if(*npartons == 2) norm=luminosity/( nevtsProcessed2Jet/xsec2Jet + nevtsProcessedIncl/xsecIncl );
-	  else if(*npartons == 3) norm=luminosity/( nevtsProcessed3Jet/xsec3Jet + nevtsProcessedIncl/xsecIncl );
-	  else if(*npartons == 4) norm=luminosity/( nevtsProcessed4Jet/xsec4Jet + nevtsProcessedIncl/xsecIncl );
-	  else                    norm=luminosity/( nevtsProcessedIncl/xsecIncl );
+
+	  norm =0;
+	  double add_NEvtsOverXsec = 0;
+
+	  add_NEvtsOverXsec += nevtsProcessedIncl/xsecIncl;
+
+	  if(*npartons==1)        add_NEvtsOverXsec += nevtsProcessed1Jet/xsec1Jet;
+	  else if(*npartons==2)   add_NEvtsOverXsec += nevtsProcessed2Jet/xsec2Jet;
+	  else if(*npartons==3)   add_NEvtsOverXsec += nevtsProcessed3Jet/xsec3Jet;
+	  else if(*npartons==4)   add_NEvtsOverXsec += nevtsProcessed4Jet/xsec4Jet;
+
+	  if(*npartonsNLO==1){
+	    if      ( *lheWPt >= 100  && *lheWPt < 150 ) add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_100to150/xsec1JetLHEWpT_100to150;
+	    else if ( *lheWPt >= 150  && *lheWPt < 250 ) add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_150to250/xsec1JetLHEWpT_150to250;
+	    else if ( *lheWPt >= 250  && *lheWPt < 400 ) add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_250to400/xsec1JetLHEWpT_250to400;
+	    else if ( *lheWPt >= 400 )                   add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_400toinf/xsec1JetLHEWpT_400toinf;
+	  }
+
+	  if(*WMass >= 200) add_NEvtsOverXsec += nevtsProcessedWToMuNu_M200/xsecWToMuNu_M200;
+
+	  if(add_NEvtsOverXsec !=0 ) norm = luminosity/add_NEvtsOverXsec;
+
 	}
-	
+
 	// CUTS
-
-	if(*mht < 60 )         continue;
+	if(*IsW != true)   continue; // this includes an mtmuon cut of 40GeV
+	if(*mtmuon < 50)   continue;
+	if(*mhtNoMu < 60 )         continue;
 	if(*muonPt < 30)       continue;
-	if(*dPhiMetMuon > 1.5) continue;
+	//if(*dPhiMetMuon > 1.5) continue;
 
-	histo_den  -> Fill( *met, weight*norm );
+	histo_den  -> Fill( *metNoMu, weight*norm );
 	
 	if(*trigger != true)   continue;
 
-	histo_num  -> Fill( *met, weight*norm );
+	histo_num  -> Fill( *metNoMu, weight*norm );
       }
     }
     histoMap[samples[i].first + "_num"] = (TH1D*) histo_num -> Clone();
@@ -185,7 +217,7 @@ void TriggerEfficiency() {
 
   TCanvas * canv = new TCanvas("canv","",1000,700);
   effData->GetYaxis()->SetTitle("Trigger Efficiency");
-  effData->GetXaxis()->SetTitle("E_{T}^{mis} [GeV]");
+  effData->GetXaxis()->SetTitle("E_{T, no #mu}^{mis} [GeV]");
   effData->GetYaxis()->SetTitleOffset(1.2);
   effData->GetXaxis()->SetTitleOffset(1.1);
   effData->SetMaximum(1.0);
