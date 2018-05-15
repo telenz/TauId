53be6b2fd83485fc168936b898b264e0a27560a6
Teresa <teresa.lenz@desy.de>
2018-05-15 14:22:59 +0200

    remove lheWPt samples since stitching can only work with NLO samples + remove WToMuNu sample

--- TriggerEfficiency.C
+++ TriggerEfficiency.C
@@ -17,11 +17,6 @@ void TriggerEfficiency() {
   MC.push_back("W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8");
   MC.push_back("W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8");
   MC.push_back("W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8");
-  MC.push_back("W1JetsToLNu_LHEWpT_100-150");
-  MC.push_back("W1JetsToLNu_LHEWpT_150-250");
-  MC.push_back("W1JetsToLNu_LHEWpT_250-400");
-  MC.push_back("W1JetsToLNu_LHEWpT_400-inf");
-  MC.push_back("WToMuNu_M-200_TuneCP5_13TeV-pythia8");
   std::vector<string> Data;
   Data.push_back("SingleMuon_Run2017");
   samples.push_back(make_pair("MC"   , MC));
@@ -34,21 +29,11 @@ void TriggerEfficiency() {
   double xsec2Jet = xsecs["W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"];
   double xsec3Jet = xsecs["W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"];
   double xsec4Jet = xsecs["W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"];
-  double xsec1JetLHEWpT_100to150 = xsecs["W1JetsToLNu_LHEWpT_100-150"];
-  double xsec1JetLHEWpT_150to250 = xsecs["W1JetsToLNu_LHEWpT_150-250"];
-  double xsec1JetLHEWpT_250to400 = xsecs["W1JetsToLNu_LHEWpT_250-400"];
-  double xsec1JetLHEWpT_400toinf = xsecs["W1JetsToLNu_LHEWpT_400-inf"];
-  double xsecWToMuNu_M200 = xsecs["WToMuNu_M-200_TuneCP5_13TeV-pythia8"];
   double nevtsProcessedIncl = getNEventsProcessed(dir+"WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed1Jet = getNEventsProcessed(dir+"W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed2Jet = getNEventsProcessed(dir+"W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed3Jet = getNEventsProcessed(dir+"W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
   double nevtsProcessed4Jet = getNEventsProcessed(dir+"W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8.root");
-  double nevtsProcessed1JetLHEWpT_100to150 = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_100-150.root");
-  double nevtsProcessed1JetLHEWpT_150to250 = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_150-250.root");
-  double nevtsProcessed1JetLHEWpT_250to400 = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_250-400.root");
-  double nevtsProcessed1JetLHEWpT_400toinf = getNEventsProcessed(dir+"W1JetsToLNu_LHEWpT_400-inf.root");
-  double nevtsProcessedWToMuNu_M200 = getNEventsProcessed(dir+"WToMuNu_M-200_TuneCP5_13TeV-pythia8.root");
 
   std::map<string,TGraphAsymmErrors*> effMapData;
   std::map<string,TGraphAsymmErrors*> effMapMC;
@@ -97,10 +82,18 @@ void TriggerEfficiency() {
 
       cout<<"---------- Processing  "<<samples[i].second[idx_list]<<"  ---------- "<<endl;
 
+      double xsec = getXSec(samples[i].second[idx_list]);
+      double nevtsProcessed = getNEventsProcessed(dir + samples[i].second[idx_list] + ".root");
+      double norm = luminosity*xsec/nevtsProcessed;
+
       while(myReader->Next()){
 
 	double weight = (*genWeight)*(*puWeight);
-	double norm   = 1.;
+
+	if( boost::contains(samples[i].first,"Data") ){
+	  weight = 1;
+	  norm = 1;
+	}
 
 	// STITCHING
 	if( boost::contains(samples[i].first,"MC") && samples[i].second.size()>1){
@@ -114,15 +107,6 @@ void TriggerEfficiency() {
 	  else if(*npartons==2)   add_NEvtsOverXsec += nevtsProcessed2Jet/xsec2Jet;
 	  else if(*npartons==3)   add_NEvtsOverXsec += nevtsProcessed3Jet/xsec3Jet;
 	  else if(*npartons==4)   add_NEvtsOverXsec += nevtsProcessed4Jet/xsec4Jet;
-
-	  if(*npartonsNLO==1){
-	    if      ( *lheWPt >= 100  && *lheWPt < 150 ) add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_100to150/xsec1JetLHEWpT_100to150;
-	    else if ( *lheWPt >= 150  && *lheWPt < 250 ) add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_150to250/xsec1JetLHEWpT_150to250;
-	    else if ( *lheWPt >= 250  && *lheWPt < 400 ) add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_250to400/xsec1JetLHEWpT_250to400;
-	    else if ( *lheWPt >= 400 )                   add_NEvtsOverXsec += nevtsProcessed1JetLHEWpT_400toinf/xsec1JetLHEWpT_400toinf;
-	  }
-
-	  if(*WMass >= 200) add_NEvtsOverXsec += nevtsProcessedWToMuNu_M200/xsecWToMuNu_M200;
 
 	  if(add_NEvtsOverXsec !=0 ) norm = luminosity/add_NEvtsOverXsec;
 
