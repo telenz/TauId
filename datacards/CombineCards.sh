125f51a700c90755c1e1a6c061fa5bd72e0c6a3e
Mareike Meyer <mareike.meyer@desy.de>
2018-06-22 11:08:12 +0200

    updated WPs in CombineCards.sh

--- datacards/CombineCards.sh
+++ datacards/CombineCards.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 tauDecayMode=""
-combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_LooseMva_WToTauNu${tauDecayMode}.txt > datacard_LooseMva_Combined${tauDecayMode}.txt
-combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_MediumMva_WToTauNu${tauDecayMode}.txt > datacard_MediumMva_Combined${tauDecayMode}.txt
-combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_TightMva_WToTauNu${tauDecayMode}.txt > datacard_TightMva_Combined${tauDecayMode}.txt
-combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_VTightMva_WToTauNu${tauDecayMode}.txt > datacard_VTightMva_Combined${tauDecayMode}.txt
+combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_LooseMva2017v2_WToTauNu${tauDecayMode}.txt > datacard_LooseMva2017v2_Combined${tauDecayMode}.txt
+combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_MediumMva2017v2_WToTauNu${tauDecayMode}.txt > datacard_MediumMva2017v2_Combined${tauDecayMode}.txt
+combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_TightMva2017v2_WToTauNu${tauDecayMode}.txt > datacard_TightMva2017v2_Combined${tauDecayMode}.txt
+combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_VTightMva2017v2_WToTauNu${tauDecayMode}.txt > datacard_VTightMva2017v2_Combined${tauDecayMode}.txt
 combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_Loose_WToTauNu${tauDecayMode}.txt > datacard_Loose_Combined${tauDecayMode}.txt
 combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_Medium_WToTauNu${tauDecayMode}.txt > datacard_Medium_Combined${tauDecayMode}.txt
 combineCards.py ../output/datacard_mtmuon_WToMuNu.txt ../output/datacard_mttau_Tight_WToTauNu${tauDecayMode}.txt > datacard_Tight_Combined${tauDecayMode}.txt
