fa96110570fc0777048a4e2e4a9fa7a0738a6087
Teresa <teresa.lenz@desy.de>
2018-09-24 14:35:53 +0200

    filename changed from mlfit.root to fitDiagnostics.root

--- datacards/Fitting.sh
+++ datacards/Fitting.sh
@@ -2,4 +2,4 @@
 # $2 tauDecayMode (_3prong0pizeros, _1prong0pizeros, _1prongUpTo4pizeros or empty)
 
 combine -M MaxLikelihoodFit --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-2 --rMax=3 -m 81 datacard_$1_Combined$2.txt -v2
-mv mlfit.root  mlfit_Combined_$1$2.root
+mv fitDiagnostics.root  fitDiagnostics_$1$2.root
