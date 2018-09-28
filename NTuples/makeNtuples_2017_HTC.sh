766fe25abc99714148fb290b679d3a54b8d5008f
Teresa <teresa.lenz@desy.de>
2018-09-28 11:16:05 +0200

    adopt number of files to consider number of entries in trees

--- NTuples/makeNtuples_2017_HTC.sh
+++ NTuples/makeNtuples_2017_HTC.sh
@@ -10,7 +10,7 @@
 ./HTC_submit_seq.sh AnalysisNTupleProducer_TauID analysisNTupleProducer_tauid_MC_W4Jets.conf W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
 
 # For fake rate (no trigger efficiencies need to be included)
-./HTC_submit_seq.sh AnalysisNTupleProducer_TauID analysisNTupleProducer_tauid_Data.conf JetHT_Run2017 10
+./HTC_submit_seq.sh AnalysisNTupleProducer_TauID analysisNTupleProducer_tauid_Data.conf JetHT_Run2017 20
 
 ./HTC_submit_seq.sh AnalysisNTupleProducer_TauID analysisNTupleProducer_tauid_MC_W1JetspT100-150.conf W1JetsToLNu_LHEWpT_100-150 20
 ./HTC_submit_seq.sh AnalysisNTupleProducer_TauID analysisNTupleProducer_tauid_MC_W1JetspT150-250.conf W1JetsToLNu_LHEWpT_150-250 20
