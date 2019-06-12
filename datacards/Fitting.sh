609174b48afb8d9a1da3f21c6cf8802561922b60
Teresa <teresa.lenz@desy.de>
2019-06-12 19:59:30 +0200

    change rmin and rmax

--- datacards/Fitting.sh
+++ datacards/Fitting.sh
@@ -1,5 +1,5 @@
 # $1 working point (e.g. VTightMva)
 # $2 tauDecayMode (_3prong0pizeros, _1prong0pizeros, _1prongUpTo4pizeros or empty)
 
-combine -M MaxLikelihoodFit --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-5 --rMax=5 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0 --robustHesse 1
+combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0 --robustHesse 1
 mv fitDiagnostics.root  fitDiagnostics_$1$2.root
