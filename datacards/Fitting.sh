4f76fc39c0f1a27c9fb98a5b6f83e73845c185e3
Teresa <teresa.lenz@desy.de>
2019-06-12 17:53:29 +0200

    add robustHesse option

--- datacards/Fitting.sh
+++ datacards/Fitting.sh
@@ -1,5 +1,5 @@
 # $1 working point (e.g. VTightMva)
 # $2 tauDecayMode (_3prong0pizeros, _1prong0pizeros, _1prongUpTo4pizeros or empty)
 
-combine -M MaxLikelihoodFit --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-2 --rMax=3 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0
+combine -M MaxLikelihoodFit --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-5 --rMax=5 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0 --robustHesse 1
 mv fitDiagnostics.root  fitDiagnostics_$1$2.root
