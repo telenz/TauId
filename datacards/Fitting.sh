eeb280eaedd0dcea24532f1189590442b544604b
Teresa <teresa.lenz@desy.de>
2019-06-12 13:24:39 +0200

    add option to combine fit to avoid failing uncertainty determination

--- datacards/Fitting.sh
+++ datacards/Fitting.sh
@@ -1,5 +1,5 @@
 # $1 working point (e.g. VTightMva)
 # $2 tauDecayMode (_3prong0pizeros, _1prong0pizeros, _1prongUpTo4pizeros or empty)
 
-combine -M MaxLikelihoodFit --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-2 --rMax=3 -m 81 datacard_$1_Combined$2.txt -v2
+combine -M MaxLikelihoodFit --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-2 --rMax=3 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0
 mv fitDiagnostics.root  fitDiagnostics_$1$2.root
