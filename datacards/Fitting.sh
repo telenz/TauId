083fb5d60b9b7517e3d8a52d3791309459ce1742
Teresa <teresa.lenz@desy.de>
2019-06-13 18:13:38 +0200

    change rMin + add seed

--- datacards/Fitting.sh
+++ datacards/Fitting.sh
@@ -3,7 +3,7 @@
 
 #combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2
 #combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0 --robustHesse 1
-combine -M FitDiagnostics --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1.0 --rMax=2.5 -m 81 datacard_$1_Combined$2.txt --redefineSignalPOIs tauId --setParameters r=1.0 #-v2
+combine -M FitDiagnostics --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=0.0 --rMax=2.5 -m 81 datacard_$1_Combined$2.txt --redefineSignalPOIs tauId --setParameters r=1.0 -s 1234
 mv fitDiagnostics.root  fitDiagnostics_$1$2.root
 
 
