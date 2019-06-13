ee1ffab6d4abc5b61d6566ae954b55e214e7c487
Teresa <teresa.lenz@desy.de>
2019-06-13 16:22:58 +0200

    switch parameters of interest for more stable fit

--- datacards/Fitting.sh
+++ datacards/Fitting.sh
@@ -1,5 +1,11 @@
 # $1 working point (e.g. VTightMva)
 # $2 tauDecayMode (_3prong0pizeros, _1prong0pizeros, _1prongUpTo4pizeros or empty)
 
-combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0 --robustHesse 1
+#combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2
+#combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0 --robustHesse 1
+combine -M FitDiagnostics --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1.0 --rMax=2.5 -m 81 datacard_$1_Combined$2.txt --redefineSignalPOIs tauId --setParameters r=1.0 #-v2
 mv fitDiagnostics.root  fitDiagnostics_$1$2.root
+
+
+# redefien signal pois
+# multiDimFit 
