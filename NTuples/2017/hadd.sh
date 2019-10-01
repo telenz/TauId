47c26d2bff23ef13b5245902d4b3f92a4392d9ec
Teresa <teresa.lenz@desy.de>
2019-10-01 12:56:47 +0200

    hadd.sh and new_setup.sh added

---
+++ NTuples/2017/hadd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for dir in $(find -maxdepth 1 -type d -name "*_files")
+do
+    filename=${dir:2:${#dir}-8}
+    echo $filename
+    if [ "$filename" == "SingleMuon_Run2017" ]; then
+        for i in {0..9}
+        do
+            hadd -f ${filename}_${i}.root ${filename}_files/*${i}.root
+        done
+    else
+        hadd -f ${filename}.root ${filename}_files/*.root
+    fi
+    checkRoot ${filename}.root
+    exit=$?
+    if [[ $exit -ne 0 ]]; then
+        echo "File ${filename}.root is corrupted. Quitting..." 
+        exit 1
+    fi
+done
+
