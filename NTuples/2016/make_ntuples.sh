ccf211590c2ba6f544260ef50794ffe093c7a527
Teresa <teresa.lenz@desy.de>
2019-10-08 13:52:54 +0200

    scripts needed for gc submission

---
+++ NTuples/2016/make_ntuples.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo @CONFIGFILE@
+
+NtupleMaker_TauID_all_eras @CONFIGFILE@ @FILELIST@
+
---
+++ NTuples/2016/new_setup.sh
@@ -0,0 +1,5 @@
+#!bin/sh
+
+source make_filelists.sh
+source make_configs_tauid.sh
+source make_parameter_file.sh
