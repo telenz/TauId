187b05a502d7a532b4e3be91662b18f3efd7a4bc
Teresa <teresa.lenz@desy.de>
2019-10-08 14:26:53 +0200

    switch to gc setup for analysis ntuple production

--- NTuples/2016/hadd.sh
+++ NTuples/2016/hadd.sh
@@ -5,18 +5,18 @@ do
     filename=${dir:2:${#dir}-8}
     echo $filename
     if [ "$filename" == "SingleMuon_Run2017" ]; then
-	for i in {0..9}
-	do
-	    hadd -f ${filename}_${i}.root ${filename}_files/*${i}.root
-	done
+        for i in {0..9}
+        do
+            hadd -f ${filename}_${i}.root ${filename}_files/*${i}.root
+        done
     else
-	hadd -f ${filename}.root ${filename}_files/*.root
+        hadd -f ${filename}.root ${filename}_files/*.root
     fi
-   checkRoot ${filename}.root
-	exit=$?
-	if [[ $exit -ne 0 ]]; then
-       echo "File ${filename}.root is corrupted. Quitting..." 
-       exit 1
-   fi
+    checkRoot ${filename}.root
+    exit=$?
+    if [[ $exit -ne 0 ]]; then
+        echo "File ${filename}.root is corrupted. Quitting..."
+        exit 1
+    fi
 done
 
--- NTuples/2016/make_filelists.sh
+++ NTuples/2016/make_filelists.sh
@@ -2,10 +2,10 @@
 
 # this script creates filelists for 2016-legacy data
 
-dirDataSingleMuon=/pnfs/desy.de/cms/tier2/store/user/telenz/13TeV/NTuples/2016/2016-legacy/data/Run2016-17Jul2018/SingleMuon/
-dirDataJetHT=/pnfs/desy.de/cms/tier2/store/user/telenz/13TeV/NTuples/2016/2016-legacy/data/Run2016-17Jul2018/JetHT/
-dirDataMET=/pnfs/desy.de/cms/tier2/store/user/telenz/13TeV/NTuples/2016/2016-legacy/data/Run2016-17Jul2018/MET/
-dirMC=/pnfs/desy.de/cms/tier2/store/user/telenz/13TeV/NTuples/2016/2016-legacy/mc/RunIISummer16MiniAODv3/
+dirDataSingleMuon=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2016/data/SingleMuon/
+dirDataJetHT=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2016/data/JetHT/
+dirDataMET=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2016/data/MET/
+dirMC=/pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2016/mc/
 
 ls $dirDataSingleMuon/SingleMuon_Run2016B-17Jul2018*/*root > SingleMuon_Run2016B
 ls $dirDataSingleMuon/SingleMuon_Run2016C-17Jul2018*/*root > SingleMuon_Run2016C
@@ -35,14 +35,14 @@ ls $dirMC/WToMuNu_M-200_TuneCUETP8M1_13TeV-pythia8/*root > WToMuNu_M-200
 ls $dirMC/WToTauNu_M-200_TuneCUETP8M1_13TeV-pythia8-tauola/*root > WToTauNu_M-200
 
 ls $dirMC/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu
-ls $dirMC/WJetsToLNu_HT-70To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-70To100
-ls $dirMC/WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-100To200
-ls $dirMC/WJetsToLNu_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-200To400
-ls $dirMC/WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-400To600
-ls $dirMC/WJetsToLNu_HT-600To800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-600To800
-ls $dirMC/WJetsToLNu_HT-800To1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-800To1200
-ls $dirMC/WJetsToLNu_HT-1200To2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-1200To2500
-ls $dirMC/WJetsToLNu_HT-2500ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > WJetsToLNu_HT-2500ToInf
+ls $dirMC/WJetsToLNu_HT-70To100*/*root > WJetsToLNu_HT-70To100
+ls $dirMC/WJetsToLNu_HT-100To200*/*root > WJetsToLNu_HT-100To200
+ls $dirMC/WJetsToLNu_HT-200To400*/*root > WJetsToLNu_HT-200To400
+ls $dirMC/WJetsToLNu_HT-400To600*/*root > WJetsToLNu_HT-400To600
+ls $dirMC/WJetsToLNu_HT-600To800*/*root > WJetsToLNu_HT-600To800
+ls $dirMC/WJetsToLNu_HT-800To1200*/*root > WJetsToLNu_HT-800To1200
+ls $dirMC/WJetsToLNu_HT-1200To2500*/*root > WJetsToLNu_HT-1200To2500
+ls $dirMC/WJetsToLNu_HT-2500ToInf*/*root > WJetsToLNu_HT-2500ToInf
 ls $dirMC/W1JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > W1JetsToLNu
 ls $dirMC/W2JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > W2JetsToLNu
 ls $dirMC/W3JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*/*root > W3JetsToLNu
@@ -64,10 +64,10 @@ ls $dirMC/ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph*/*root > ZJetsToNuNu_HT-2500To
 
 ls $dirMC/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/*root > TT
 
-ls $dirMC/ST_t-channel_antitop_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8_TuneCUETP8M1*/*root > ST_t-channel_top_4f_inclusiveDecays
-ls $dirMC/ST_t-channel_top_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8_TuneCUETP8M1*/*root > ST_t-channel_antitop_4f_inclusiveDecays
-ls $dirMC/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1*/*root > ST_tW_antitop_5f_inclusiveDecays
-ls $dirMC/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1*/*root > ST_tW_top_5f_inclusiveDecays
+ls $dirMC/ST_t-channel_antitop_4f*/*root > ST_t-channel_top_4f_inclusiveDecays
+ls $dirMC/ST_t-channel_top_4f*/*root > ST_t-channel_antitop_4f_inclusiveDecays
+ls $dirMC/ST_tW_antitop_5f*/*root > ST_tW_antitop_5f_inclusiveDecays
+ls $dirMC/ST_tW_top_5f*/*root > ST_tW_top_5f_inclusiveDecays
 
 ls $dirMC/WW_TuneCUETP8M1_13TeV-pythia8*/*root > WW
 ls $dirMC/WZ_TuneCUETP8M1_13TeV-pythia8*/*root > WZ
