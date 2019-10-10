abc77cd047a164485e8360d980587ee80b34bbbe
Teresa <teresa.lenz@desy.de>
2019-10-10 11:24:51 +0200

    uncomment lines

--- NTuples/2016/make_parameter_file.sh
+++ NTuples/2016/make_parameter_file.sh
@@ -3,21 +3,6 @@
 echo "CONFIGFILE,FILELIST" > parameters.txt
 
 # For trigger efficiencies
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016B 20
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016C 20
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016D 20
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016E 20
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016F 20
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016G 20
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016H 20
-
-./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W1JetsToLNu 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W2JetsToLNu 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W3JetsToLNu 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W4JetsToLNu 40
-
-# # For the rest of measurement
 # ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016B 20
 # ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016C 20
 # ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016D 20
@@ -26,95 +11,110 @@ echo "CONFIGFILE,FILELIST" > parameters.txt
 # ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016G 20
 # ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016H 20
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016B 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016C 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016D 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016E 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016F 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016G 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016H 40
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu 40
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W1JetsToLNu 40
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W2JetsToLNu 40
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W3JetsToLNu 40
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W4JetsToLNu 40
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016B 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016C 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016D 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016E 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016F 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016G 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016H 40
+# # For the rest of measurement
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016B 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016C 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016D 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016E 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016F 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016G 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf SingleMuon_Run2016H 20
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W1JetsToLNu 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W2JetsToLNu 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W3JetsToLNu 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf W4JetsToLNu 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-70To100 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-100To200 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-200To400 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-400To600 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-600To800 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-800To1200 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-1200To2500 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WJetsToLNu_HT-2500ToInf 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016B 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016C 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016D 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016E 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016F 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016G 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf JetHT_Run2016H 40
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZJetsToNuNu_HT-100To200 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZJetsToNuNu_HT-200To400 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZJetsToNuNu_HT-400To600 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZJetsToNuNu_HT-600To800 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZJetsToNuNu_HT-800To1200 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZJetsToNuNu_HT-1200To2500 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZJetsToNuNu_HT-2500ToInf 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016B 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016C 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016D 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016E 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016F 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016G 40
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2016.conf MET_Run2016H 40
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf DYJetsToLL_M-50 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf DY1JetsToLL_M-50 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf DY2JetsToLL_M-50 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf DY3JetsToLL_M-50 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf DY4JetsToLL_M-50 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W1JetsToLNu 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W2JetsToLNu 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W3JetsToLNu 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf W4JetsToLNu 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-70To100 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-100To200 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-200To400 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-400To600 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-600To800 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-800To1200 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-1200To2500 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WJetsToLNu_HT-2500ToInf 40
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf TT 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZJetsToNuNu_HT-100To200 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZJetsToNuNu_HT-200To400 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZJetsToNuNu_HT-400To600 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZJetsToNuNu_HT-600To800 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZJetsToNuNu_HT-800To1200 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZJetsToNuNu_HT-1200To2500 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZJetsToNuNu_HT-2500ToInf 40
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ST_t-channel_top_4f_inclusiveDecays 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ST_t-channel_antitop_4f_inclusiveDecays 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ST_tW_antitop_5f_inclusiveDecays 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ST_tW_top_5f_inclusiveDecays 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf DYJetsToLL_M-50 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf DY1JetsToLL_M-50 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf DY2JetsToLL_M-50 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf DY3JetsToLL_M-50 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf DY4JetsToLL_M-50 40
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WW 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WZ 40
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf ZZ 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf TT 40
 
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WToMuNu_M-200 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC.conf WToTauNu_M-200 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ST_t-channel_top_4f_inclusiveDecays 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ST_t-channel_antitop_4f_inclusiveDecays 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ST_tW_antitop_5f_inclusiveDecays 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ST_tW_top_5f_inclusiveDecays 40
 
-# # For uncertainties
-# cp WToMuNu_M-200 WToMuNu_M-200_jesUp
-# cp WToMuNu_M-200 WToMuNu_M-200_jesDown
-# cp WToMuNu_M-200 WToMuNu_M-200_uesUp
-# cp WToMuNu_M-200 WToMuNu_M-200_uesDown
-# cp WToMuNu_M-200 WToMuNu_M-200_muUp
-# cp WToMuNu_M-200 WToMuNu_M-200_muDown
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_jesUp_WStarMuNu.conf WToMuNu_M-200_jesUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_jesDown_WStarMuNu.conf WToMuNu_M-200_jesDown 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_uesUp_WStarMuNu.conf WToMuNu_M-200_uesUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_uesDown_WStarMuNu.conf WToMuNu_M-200_uesDown 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_muUp_WStarMuNu.conf WToMuNu_M-200_muUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_muDown_WStarMuNu.conf WToMuNu_M-200_muDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WW 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WZ 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf ZZ 40
 
-# cp WToTauNu_M-200 WToTauNu_M-200_jesUp
-# cp WToTauNu_M-200 WToTauNu_M-200_jesDown
-# cp WToTauNu_M-200 WToTauNu_M-200_uesUp
-# cp WToTauNu_M-200 WToTauNu_M-200_uesDown
-# cp WToTauNu_M-200 WToTauNu_M-200_taues_1prong0pizerosUp
-# cp WToTauNu_M-200 WToTauNu_M-200_taues_1prong0pizerosDown
-# cp WToTauNu_M-200 WToTauNu_M-200_taues_1prongUpTo4pizerosUp
-# cp WToTauNu_M-200 WToTauNu_M-200_taues_1prongUpTo4pizerosDown
-# cp WToTauNu_M-200 WToTauNu_M-200_taues_3prong0pizerosUp
-# cp WToTauNu_M-200 WToTauNu_M-200_taues_3prong0pizerosDown
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prong0pizerosDown_WStarTauNu.conf WToTauNu_M-200_taues_1prong0pizerosDown 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prong0pizerosUp_WStarTauNu.conf  WToTauNu_M-200_taues_1prong0pizerosUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prongUpTo4pizerosDown_WStarTauNu.conf WToTauNu_M-200_taues_1prongUpTo4pizerosDown 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prongUpTo4pizerosUp_WStarTauNu.conf WToTauNu_M-200_taues_1prongUpTo4pizerosUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_3prong0pizerosDown_WStarTauNu.conf WToTauNu_M-200_taues_3prong0pizerosDown 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_3prong0pizerosUp_WStarTauNu.conf WToTauNu_M-200_taues_3prong0pizerosUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_jesUp_WStarTauNu.conf WToTauNu_M-200_jesUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_jesDown_WStarTauNu.conf WToTauNu_M-200_jesDown 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_uesUp_WStarTauNu.conf WToTauNu_M-200_uesUp 20
-# ./split_filelist.sh analysisNTupleProducer_tauid_MC_uesDown_WStarTauNu.conf WToTauNu_M-200_uesDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WToMuNu_M-200 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2016.conf WToTauNu_M-200 20
+
+# For uncertainties
+cp WToMuNu_M-200 WToMuNu_M-200_jesUp
+cp WToMuNu_M-200 WToMuNu_M-200_jesDown
+cp WToMuNu_M-200 WToMuNu_M-200_uesUp
+cp WToMuNu_M-200 WToMuNu_M-200_uesDown
+cp WToMuNu_M-200 WToMuNu_M-200_muUp
+cp WToMuNu_M-200 WToMuNu_M-200_muDown
+./split_filelist.sh analysisNTupleProducer_tauid_MC_jesUp_WStarMuNu.conf WToMuNu_M-200_jesUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_jesDown_WStarMuNu.conf WToMuNu_M-200_jesDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_uesUp_WStarMuNu.conf WToMuNu_M-200_uesUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_uesDown_WStarMuNu.conf WToMuNu_M-200_uesDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_muUp_WStarMuNu.conf WToMuNu_M-200_muUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_muDown_WStarMuNu.conf WToMuNu_M-200_muDown 20
+
+cp WToTauNu_M-200 WToTauNu_M-200_jesUp
+cp WToTauNu_M-200 WToTauNu_M-200_jesDown
+cp WToTauNu_M-200 WToTauNu_M-200_uesUp
+cp WToTauNu_M-200 WToTauNu_M-200_uesDown
+cp WToTauNu_M-200 WToTauNu_M-200_taues_1prong0pizerosUp
+cp WToTauNu_M-200 WToTauNu_M-200_taues_1prong0pizerosDown
+cp WToTauNu_M-200 WToTauNu_M-200_taues_1prongUpTo4pizerosUp
+cp WToTauNu_M-200 WToTauNu_M-200_taues_1prongUpTo4pizerosDown
+cp WToTauNu_M-200 WToTauNu_M-200_taues_3prong0pizerosUp
+cp WToTauNu_M-200 WToTauNu_M-200_taues_3prong0pizerosDown
+./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prong0pizerosDown_WStarTauNu.conf WToTauNu_M-200_taues_1prong0pizerosDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prong0pizerosUp_WStarTauNu.conf  WToTauNu_M-200_taues_1prong0pizerosUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prongUpTo4pizerosDown_WStarTauNu.conf WToTauNu_M-200_taues_1prongUpTo4pizerosDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_1prongUpTo4pizerosUp_WStarTauNu.conf WToTauNu_M-200_taues_1prongUpTo4pizerosUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_3prong0pizerosDown_WStarTauNu.conf WToTauNu_M-200_taues_3prong0pizerosDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_taues_3prong0pizerosUp_WStarTauNu.conf WToTauNu_M-200_taues_3prong0pizerosUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_jesUp_WStarTauNu.conf WToTauNu_M-200_jesUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_jesDown_WStarTauNu.conf WToTauNu_M-200_jesDown 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_uesUp_WStarTauNu.conf WToTauNu_M-200_uesUp 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_uesDown_WStarTauNu.conf WToTauNu_M-200_uesDown 20
