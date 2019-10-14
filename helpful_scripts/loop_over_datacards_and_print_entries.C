ef76d67fe01609f93bdbd4f15f7c9dc339b8181e
Teresa <teresa.lenz@desy.de>
2019-10-14 16:35:08 +0200

    set relative paths correct

--- helpful_scripts/loop_over_datacards_and_print_entries.C
+++ helpful_scripts/loop_over_datacards_and_print_entries.C
@@ -1,11 +1,11 @@
-#include "HttStylesNew.cc"
-#include "settings.h"
+#include "../HttStylesNew.cc"
+#include "../settings.h"
 #include <sstream> 
 
 void loop_over_datacards_and_print_entries() {
 
 
-  TFile *in = new TFile("output/mtmuon_WToMuNu_shapes.root","READ");
+  TFile *in = new TFile("../output/mttau_TightDeepTau2017v2p1_WToTauNu_shapes.root","READ");
   //TFile *in = new TFile("output/mttau_TightMva2017v2_WToTauNu_shapes.root","READ");
   TIter next(in->GetListOfKeys());
   TKey *key = 0;
