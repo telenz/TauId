1a6dbb541903bf8c6c993006709731ff81bdfa51
Teresa <teresa.lenz@desy.de>
2019-10-15 08:41:47 +0200

    no change in the fit options but just re-written for better readibility

--- datacards/Fitting.sh
+++ datacards/Fitting.sh
@@ -3,7 +3,18 @@
 
 #combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2
 #combine -M MaxLikelihoodFit --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=-1 --rMax=1 -m 81 datacard_$1_Combined$2.txt -v2 --cminDefaultMinimizerStrategy 0 --robustHesse 1
-combine -M FitDiagnostics --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=0.4 --rMax=2.5 -m 81 datacard_$1_Combined$2.txt --redefineSignalPOIs tauId --setParameters r=1.0 -s 1234
+combine -M FitDiagnostics \
+    --saveNormalizations \
+    --saveShapes \
+    --saveWithUncertainties \
+    --saveNLL \
+    --robustFit=1 \
+    --rMin=0.4 \
+    --rMax=2.5 \
+    -m 81 datacard_$1_Combined$2.txt \
+    --redefineSignalPOIs tauId \
+    --setParameters r=1.0 \
+    -s 1234
 
 # some scale factors were determined with rMin=0.0, some with rMin=0.3
 
