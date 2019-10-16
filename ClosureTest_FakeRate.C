deb128eb0f2c4f026fcb8fdea619d2e5a79b22f2
Teresa <teresa.lenz@desy.de>
2019-10-16 11:07:33 +0200

    change binning in closure (commented out)

--- ClosureTest_FakeRate.C
+++ ClosureTest_FakeRate.C
@@ -43,14 +43,14 @@ void ClosureTest_FakeRate() {
 
   //Float_t bins[] = {100,150,200,250,300,400,500};  // tauPt binning
   //Float_t bins[] = {100,200,300,400,500,700};  // met binning
-  //Float_t bins[] = {100,140,180,220,260,300,340,380,420,460,500,600,800}; // tauJetPt + tauPt + met fine binning
+  //Float_t bins[] = {180,220,260,300,340,380,420,460,500,560,620,700,800}; // tauJetPt + tauPt + met fine binning
   Float_t bins[] = {200,300,400,500,600,800};  // mttau binning
   //Float_t bins[] = {200,250,300,350,400,450,500,550,600,650,700,750,800};  // fine mttau binning
   //Float_t bins[] = {0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8};  // tauMass binning 3prong
   //Float_t bins[] = {0.0, 0.2, 0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8, 2.0, 2.2, 2.4};  // tauMass binning
   //Float_t bins[] = {0.05,0.1,0.125,0.15,0.2,0.25,0.3}; //tauMass binning 1prong 0pizeros
   //Float_t bins[] = {0,0.5,1.1,2.3}; //tauEta binning
-  //Float_t bins[] = {0.0,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0,2.0}; //ratio binning
+  //Float_t bins[] = {0.4,0.5,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0,2.0}; //ratio binning
   const int nBins = sizeof(bins)/sizeof(Float_t) - 1;
   
   for(unsigned int idx_iso=0; idx_iso<iso.size(); idx_iso++){
