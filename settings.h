cf241c52ffda196aec961c3019b109ef8f9992f0
Mareike Meyer <mareike.meyer@desy.de>
2019-10-17 10:18:44 +0200

    added mttau binning for VVLooseWP 2016 (commented out)

--- settings.h
+++ settings.h
@@ -52,6 +52,7 @@ TH1D* h_kFactor= 0;
 // Definition of mttau and mtmuon bins
 vector<Float_t> mtmuon_bins = { 100 , 200 , 300 , 400 , 500 , 600 , 700 , 1000};
 vector<Float_t> mttau_bins  = { 160 , 220 , 280 , 340 , 420 , 500 , 580 , 680 ,  800 , 1000};
+//vector<Float_t> mttau_bins  = { 160 , 220 , 280 , 340 , 400 , 480, 580, 680, 800, 1000}; //new binning for 2017 VVLooseWP
 //vector<Float_t> mttau_bins  = { 0 , 100 , 200 , 300 , 400 , 500 , 600 , 700 , 800 , 900 , 1000};
 
 //Float_t bins[] = { 200,210,220,230,240,250,260,270,280,290,300,310,320,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,480,490,500};
