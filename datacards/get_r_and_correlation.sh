873488e3f51b8ffd50639fa31da4cfea2f7017d9
Teresa <teresa.lenz@desy.de>
2019-10-17 09:49:52 +0200

    update to DeepTau Id v2p1

--- datacards/get_r_and_correlation.sh
+++ datacards/get_r_and_correlation.sh
@@ -62,44 +62,44 @@
 # fit_s->correlation("tauId","r")
 # EOF
 
-root -b -l <<EOF > results_VVLooseDeepTau2017v2.txt
-TFile *_file0 = TFile::Open("fitDiagnostics_VVLooseDeepTau2017v2.root")
+root -b -l <<EOF > results_VVLooseDeepTau2017v2p1.txt
+TFile *_file0 = TFile::Open("fitDiagnostics_VVLooseDeepTau2017v2p1.root")
 fit_s->Print()
 fit_s->correlation("tauId","r")
 EOF
 
-root -b -l <<EOF > results_VLooseDeepTau2017v2.txt
-TFile *_file0 = TFile::Open("fitDiagnostics_VLooseDeepTau2017v2.root")
+root -b -l <<EOF > results_VLooseDeepTau2017v2p1.txt
+TFile *_file0 = TFile::Open("fitDiagnostics_VLooseDeepTau2017v2p1.root")
 fit_s->Print()
 fit_s->correlation("tauId","r")
 EOF
 
-root -b -l <<EOF > results_LooseDeepTau2017v2.txt
-TFile *_file0 = TFile::Open("fitDiagnostics_LooseDeepTau2017v2.root")
+root -b -l <<EOF > results_LooseDeepTau2017v2p1.txt
+TFile *_file0 = TFile::Open("fitDiagnostics_LooseDeepTau2017v2p1.root")
 fit_s->Print()
 fit_s->correlation("tauId","r")
 EOF
 
-root -b -l <<EOF > results_MediumDeepTau2017v2.txt
-TFile *_file0 = TFile::Open("fitDiagnostics_MediumDeepTau2017v2.root")
+root -b -l <<EOF > results_MediumDeepTau2017v2p1.txt
+TFile *_file0 = TFile::Open("fitDiagnostics_MediumDeepTau2017v2p1.root")
 fit_s->Print()
 fit_s->correlation("tauId","r")
 EOF
 
-root -b -l <<EOF > results_TightDeepTau2017v2.txt
-TFile *_file0 = TFile::Open("fitDiagnostics_TightDeepTau2017v2.root")
+root -b -l <<EOF > results_TightDeepTau2017v2p1.txt
+TFile *_file0 = TFile::Open("fitDiagnostics_TightDeepTau2017v2p1.root")
 fit_s->Print()
 fit_s->correlation("tauId","r")
 EOF
 
-root -b -l <<EOF > results_VTightDeepTau2017v2.txt
-TFile *_file0 = TFile::Open("fitDiagnostics_VTightDeepTau2017v2.root")
+root -b -l <<EOF > results_VTightDeepTau2017v2p1.txt
+TFile *_file0 = TFile::Open("fitDiagnostics_VTightDeepTau2017v2p1.root")
 fit_s->Print()
 fit_s->correlation("tauId","r")
 EOF
 
-root -b -l <<EOF > results_VVTightDeepTau2017v2.txt
-TFile *_file0 = TFile::Open("fitDiagnostics_VVTightDeepTau2017v2.root")
+root -b -l <<EOF > results_VVTightDeepTau2017v2p1.txt
+TFile *_file0 = TFile::Open("fitDiagnostics_VVTightDeepTau2017v2p1.root")
 fit_s->Print()
 fit_s->correlation("tauId","r")
 EOF
@@ -107,7 +107,7 @@ EOF
 
 #wps=("Loose" "Medium" "Tight" "VVLooseMva2017v2" "VLooseMva2017v2" "LooseMva2017v2" "MediumMva2017v2" "TightMva2017v2" "VTightMva2017v2" "VVTightMva2017v2")
 #wps=("VLooseMva2017v2" "LooseMva2017v2" "MediumMva2017v2" "TightMva2017v2" "VTightMva2017v2" "VVTightMva2017v2")
-wps=("VVLooseDeepTau2017v2" "VLooseDeepTau2017v2" "LooseDeepTau2017v2" "MediumDeepTau2017v2" "TightDeepTau2017v2" "VTightDeepTau2017v2" "VVTightDeepTau2017v2")
+wps=("VVLooseDeepTau2017v2p1" "VLooseDeepTau2017v2p1" "LooseDeepTau2017v2p1" "MediumDeepTau2017v2p1" "TightDeepTau2017v2p1" "VTightDeepTau2017v2p1" "VVTightDeepTau2017v2p1")
 
 for i in "${wps[@]}"
 do
