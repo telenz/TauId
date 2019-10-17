7a1241b4db7d1fe51d53b9f5d961eab4585036d4
Teresa <teresa.lenz@desy.de>
2019-10-17 17:28:59 +0200

    change number of root files per jobs

--- NTuples/2018/make_parameter_file.sh
+++ NTuples/2018/make_parameter_file.sh
@@ -2,75 +2,75 @@
 
 echo "CONFIGFILE,FILELIST" > parameters.txt
 
-# For trigger efficiencies
-#./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018A 20
-#./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018B 20
-#./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018C 20
-#./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018D 20
+# # For trigger efficiencies
+# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018A 20
+# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018B 20
+# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018C 20
+# ./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018D 20
 
-#./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-#./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-#./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-#./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-#./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+# ./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
 
 
 # For the rest of measurement
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018A 5
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018B 5
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018C 5
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018D 5
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018A 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018B 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018C 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf SingleMuon_Run2018D 20
 
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018A 5
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018B 1
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018C 1
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018D 5
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018A 50
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018B 50
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018C 50
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf JetHT_Run2018D 50
 
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018A 5
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018B 5
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018C 5
-./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018D 5
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018A 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018B 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018C 20
+./split_filelist.sh analysisNTupleProducer_tauid_Data_2018.conf MET_Run2018D 20
 
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W1JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W2JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W3JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8 50
 
 # Wjets HT binned
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8 50
 
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-100To200_13TeV-madgraph 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-200To400_13TeV-madgraph 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-400To600_13TeV-madgraph 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-600To800_13TeV-madgraph 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-800To1200_13TeV-madgraph 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-1200To2500_13TeV-madgraph 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-100To200_13TeV-madgraph 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-200To400_13TeV-madgraph 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-400To600_13TeV-madgraph 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-600To800_13TeV-madgraph 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-800To1200_13TeV-madgraph 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-1200To2500_13TeV-madgraph 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph 50
 
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WW_TuneCP5_13TeV-pythia8 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WZ_TuneCP5_13TeV-pythia8 40
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZZ_TuneCP5_13TeV-pythia8 40
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WW_TuneCP5_13TeV-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WZ_TuneCP5_13TeV-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf ZZ_TuneCP5_13TeV-pythia8 50
 
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 20
-./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 20
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 50
+./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 50
                                                                                        
 ./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WToMuNu_M-200_TuneCP5_13TeV-pythia8 20
 ./split_filelist.sh analysisNTupleProducer_tauid_MC_2018.conf WToTauNu_M-200_TuneCP5_13TeV-pythia8-tauola 20
