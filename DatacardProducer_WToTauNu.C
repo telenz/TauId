1b862e0019503d428fbcb0c93d6dd6e34dfc5889
mmeyer <mareike.meyer@desy.de>
2020-11-20 12:40:20 +0100

    fix bug in lumi unc: only applied to bg from MC

--- DatacardProducer_WToTauNu.C
+++ DatacardProducer_WToTauNu.C
@@ -107,8 +107,8 @@ void DatacardProducer_WToTauNu(TString selection = "inclusive") {
 	textFile << setw(30) << Form("FR%i%i",i,j) << setw(15) << "shape" << setw(15) << "-" << setw(15) << "1.00" << setw(15) << "-" << endl;
       }
     }
-    textFile << setw(15) << "BkgdNorm" << setw(15) << "lnN" << setw(15) << "-"      << setw(15) << "-"      << setw(15) << "1.30"   << endl;
-    textFile << setw(15) << "lumi"     << setw(15) << "lnN" << setw(15) << lumi_unc << setw(15) << lumi_unc << setw(15) << lumi_unc << endl;
+    textFile << setw(30) << "BkgdNorm" << setw(15) << "lnN" << setw(15) << "-"      << setw(15) << "-"      << setw(15) << "1.30"   << endl;
+    textFile << setw(30) << "lumi"     << setw(15) << "lnN" << setw(15) << lumi_unc << setw(15) << "-"      << setw(15) << lumi_unc << endl;
     if (!doTauTriggerEffmeasurement) textFile << "tauId  rateParam WTauNu W  1  [0.0,2.0]" << endl;
   
     // *******************
