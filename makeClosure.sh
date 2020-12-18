1b24974bbf57a00f1d6e65545650b72fb8b34d9c
mmeyer <mareike.meyer@desy.de>
2020-12-18 17:49:56 +0100

    go back to closure for tauId measurement

--- makeClosure.sh
+++ makeClosure.sh
@@ -1,5 +1,3 @@
 
-root -l -b -q ComputeFakeRate.C+"(true,false)"
-root -l -b -q ClosureTest_FakeRate.C+"(false)"
-root -l -b -q ComputeFakeRate.C+"(true,true)"
-root -l -b -q ClosureTest_FakeRate.C+"(true)"
+root -l -b -q ComputeFakeRate.C+"(true)"
+root -l -b -q ClosureTest_FakeRate.C+
