c050cc21911ba8d4b074617530797fd7d95537cb
mmeyer <mareike.meyer@desy.de>
2020-12-18 17:56:31 +0100

    added file to make closure for trigger measurement

---
+++ makeClosure_trigger.sh
@@ -0,0 +1,5 @@
+
+root -l -b -q ComputeFakeRate.C+"(true,false)"
+root -l -b -q ClosureTest_FakeRate.C+"(false)"
+root -l -b -q ComputeFakeRate.C+"(true,true)"
+root -l -b -q ClosureTest_FakeRate.C+"(true)"
